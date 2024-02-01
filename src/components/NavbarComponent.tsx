0267a0a04f0d654ee9a4234823d205c90bde2816
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2024-02-02 00:22:08 +0530

    Completed Landing page layout (has bugs in responsive)

---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,53 @@
+"use client"
+import Link from "next/link";
+import {
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/react";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function NavbarComponent() {
+  return (
+    <div className="grid grid-cols-3 w-full bg-gradient-to-b from-black to-transparent items-center py-4 sticky top-0">
+      <span className="col-start-2 invisible xl:visible w-full flex flex-row gap-2 justify-between">
+        <Link href="/">
+          <Button className="text-base text-white bg-opacity-0">CLUBS</Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-base text-white bg-opacity-0">EVENTS</Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-base text-white bg-opacity-0">GALLERY</Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-base text-white bg-opacity-0">ABOUT</Button>
+        </Link>
+        <Link href="/">
+          <Button className="text-base text-white bg-opacity-0">FAQs</Button>
+        </Link>
+      </span>
+      <span className="justify-self-end">
+        <Dropdown backdrop="blur">
+          <DropdownTrigger>
+            <Button className="bg-opacity-0"><MenuIcon sx={{ fontSize: 40, color:"white"}} /></Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem><p className="text-2xl m-2">About Us</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">FAQs</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">Our Secretaries</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">Cultural Council</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">Technology Council</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">Gallery</p></DropdownItem>
+            <DropdownItem><p className="text-2xl m-2">Contact Us</p></DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </span>
+    </div>
+  );
+}
+
+export default NavbarComponent;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        'Gold':'#F4C95D'
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui({
+    prefix: "nextui", // prefix for themes variables
+    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+    // defaultTheme: "dark", // default theme from the themes object
+    // defaultExtendTheme: "dark", // default theme to extend on custom themes
+    layout: {}, // common layout tokens (applied to all themes)
+    themes: {
+      light: {
+        layout: {}, // light theme layout tokens
+        colors: {}, // light theme colors
+      },
+      dark: {
+        layout: {}, // dark theme layout tokens
+        colors: {}, // dark theme colors
+      },
+      // ... custom themes
+    },
+  })],
 }
-export default config
+
