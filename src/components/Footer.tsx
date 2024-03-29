c0cc44f8c969477be897cad6d09418aa5531b1bd
Aviral-Sharma-2578 <aviralshar10@gmail.com>
2024-03-29 13:59:48 +0530

    message and about

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import Link from 'next/link'
-import {Divider, Button} from "@nextui-org/react";
-import {Facebook, Instagram, LinkedIn} from '@mui/icons-material';
-import '@fontsource/poppins';
+import React from "react";
+import Link from "next/link";
+import { Divider, Button } from "@nextui-org/react";
+import { Facebook, Instagram, LinkedIn } from "@mui/icons-material";
+// import '@fontsource/poppins';
 function Footer() {
   return (
     <div className="font-poppins flex flex-col w-full text-base px-6 sm:px-12 lg:px-24 bg-white">
@@ -16,26 +17,55 @@ function Footer() {
               alt="Picture of the author"
             />
           </div>
-          <div className="text-2xl sm:text-3xl font-extrabold w-fit">Student's Gymkhana</div>
+          <div className="text-2xl sm:text-3xl font-extrabold w-fit">
+            Student's Gymkhana
+          </div>
         </div>
         <div className="flex flex-col xl:flex-row xl:h-24">
-            <div className="mx-2 my-2 xl:my-0 lg:mx-6 xl:mx-12"><h1 className="text-xl sm:text-2xl font-bold">Location</h1><p className="w-full xl:w-56">Indian Institute of Technology Indore, Khandwa Road, Simrol, Indore 453552</p></div>
-            <div className="mx-2 my-2 xl:my-0 lg:mx-6 xl:mx-2"><h1 className="text-xl sm:text-2xl font-bold">Contact</h1><p>studentgym@iiti.ac.in</p></div>
+          <div className="mx-2 my-2 xl:my-0 lg:mx-6 xl:mx-12">
+            <h1 className="text-xl sm:text-2xl font-bold">Location</h1>
+            <p className="w-full xl:w-56">
+              Indian Institute of Technology Indore, Khandwa Road, Simrol,
+              Indore 453552
+            </p>
+          </div>
+          <div className="mx-2 my-2 xl:my-0 lg:mx-6 xl:mx-2">
+            <h1 className="text-xl sm:text-2xl font-bold">Contact</h1>
+            <p>studentgym@iiti.ac.in</p>
+          </div>
         </div>
       </div>
       <Divider className="my-2 bg-slate-600" />
       <div className="xl:grid grid-cols-3 my-4 w-full items-center">
-        <p className="xl:text-left text-center">© 2024 IITI. All rights reserved.</p>
+        <p className="xl:text-left text-center">
+          © 2024 IITI. All rights reserved.
+        </p>
         <span className="invisible xl:visible justify-self-center w-64 flex flex-row gap-2 justify-between">
-            <Link href='/'><Button size='sm' className="bg-white text-base">Home</Button></Link>
-            <Link href='/'><Button size='sm' className="bg-white text-base">Clubs</Button></Link>
-            <Link href='/'><Button size='sm' className="bg-white text-base">Events</Button></Link>
-            <Link href='/'><Button size='sm' className="bg-white text-base">About</Button></Link>
+          <Link href="/">
+            <Button size="sm" className="bg-white text-base">
+              Home
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button size="sm" className="bg-white text-base">
+              Clubs
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button size="sm" className="bg-white text-base">
+              Events
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button size="sm" className="bg-white text-base">
+              About
+            </Button>
+          </Link>
         </span>
         <span className="justify-self-end flex flex-row gap-6 w-full justify-center xl:justify-end">
-            <Facebook sx={{color: "#374151", fontSize:40}}/>
-            <Instagram sx={{color: "#374151", fontSize:40}}/>
-            <LinkedIn sx={{color: "#374151", fontSize:40}}/>
+          <Facebook sx={{ color: "#374151", fontSize: 40 }} />
+          <Instagram sx={{ color: "#374151", fontSize: 40 }} />
+          <LinkedIn sx={{ color: "#374151", fontSize: 40 }} />
         </span>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 // tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,80 +8,72 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    
     extend: {
-
-      
-
-      colors:{
-        prb:'#3B82F6',
-        'Gold':'#F4C95D',
+      colors: {
+        prb: "#3B82F6",
+        Gold: "#F4C95D",
       },
       patterns: {
         opacities: {
-            100: "1",
-            80: ".80",
-            60: ".60",
-            40: ".40",
-            20: ".20",
-            10: ".10",
-            5: ".05",
+          100: "1",
+          80: ".80",
+          60: ".60",
+          40: ".40",
+          20: ".20",
+          10: ".10",
+          5: ".05",
         },
         sizes: {
-            1: "0.25rem",
-            2: "0.5rem",
-            4: "1rem",
-            6: "1.5rem",
-            8: "2rem",
-            16: "4rem",
-            20: "5rem",
-            24: "6rem",
-            32: "8rem",
+          1: "0.25rem",
+          2: "0.5rem",
+          4: "1rem",
+          6: "1.5rem",
+          8: "2rem",
+          16: "4rem",
+          20: "5rem",
+          24: "6rem",
+          32: "8rem",
         },
       },
 
-      
       animation: {
-        glow: 'glow .25s 5',
-        transition: 'all ease-in-out'
+        glow: "glow .25s 5",
+        transition: "all ease-in-out",
       },
       keyframes: {
         glow: {
-          '0%, 100%': {
-            color: 'transparent',
+          "0%, 100%": {
+            color: "transparent",
           },
-          '50%': {
-            color: 'white',
+          "50%": {
+            color: "white",
           },
         },
       },
-      
-    }
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-=======
-  
-  darkMode: "class",
-  plugins: [
-    require('tailwindcss-bg-patterns'),
-    nextui({
-    prefix: "nextui", // prefix for themes variables
-    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-    // defaultTheme: "dark", // default theme from the themes object
-    // defaultExtendTheme: "dark", // default theme to extend on custom themes
-    layout: {}, // common layout tokens (applied to all themes)
-    themes: {
-      light: {
-        layout: {}, // light theme layout tokens
-        colors: {}, // light theme colors
-      },
-      dark: {
-        layout: {}, // dark theme layout tokens
-        colors: {}, // dark theme colors
-      },
-      // ... custom themes
     },
-  })],
-}
 
+    darkMode: "class",
+    plugins: [
+      require("tailwind-scrollbar-hide"),
+      require("tailwindcss-bg-patterns"),
+      nextui({
+        prefix: "nextui", // prefix for themes variables
+        addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+        // defaultTheme: "dark", // default theme from the themes object
+        // defaultExtendTheme: "dark", // default theme to extend on custom themes
+        layout: {}, // common layout tokens (applied to all themes)
+        themes: {
+          light: {
+            layout: {}, // light theme layout tokens
+            colors: {}, // light theme colors
+          },
+          dark: {
+            layout: {}, // dark theme layout tokens
+            colors: {}, // dark theme colors
+          },
+          // ... custom themes
+        },
+      }),
+    ],
+  },
+};
