fc1d54074c1dcf4a7b423647a28e6e48550c63fe
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2024-05-12 18:38:50 +0530

    home page changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,62 @@
-// tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      colors:{
-        'Gold':'#F4C95D'
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui({
-    prefix: "nextui", // prefix for themes variables
-    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-    // defaultTheme: "dark", // default theme from the themes object
-    // defaultExtendTheme: "dark", // default theme to extend on custom themes
-    layout: {}, // common layout tokens (applied to all themes)
-    themes: {
-      light: {
-        layout: {}, // light theme layout tokens
-        colors: {}, // light theme colors
+  plugins: [
+    require("tailwindcss-animate"),
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      // defaultTheme: "light", // default theme from the themes object
+      // defaultExtendTheme: "dark", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {}, // dark theme colors
+        },
+        // ... custom themes
       },
-      dark: {
-        layout: {}, // dark theme layout tokens
-        colors: {}, // dark theme colors
-      },
-      // ... custom themes
-    },
-  })],
-}
+    }),
+  ],
+} satisfies Config;
 
+export default config;
